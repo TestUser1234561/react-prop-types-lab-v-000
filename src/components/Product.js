74e06bb38d21335c586809771880ce4c32debb12
Morgan Fudge <mfudge92@gmail.com>
2018-05-10 15:44:45 +0000

    automatically backed up by learn

--- src/components/Product.js
+++ src/components/Product.js
@@ -25,7 +25,7 @@ Product.propTypes = {
 	hasWatermark: PropTypes.bool,
 	color: PropTypes.oneOf(['white', 'eggshell-white', 'salmon']).isRequired,
 	weight: function(prop) {
-		if(prop > 300 || prop < 80) {
+		if(!(weight > 80 && weight < 300)) {
 			return new Error('Invalid prop!')
 		}
 	}
